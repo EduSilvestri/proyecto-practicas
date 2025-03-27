b78f017e093022b06584827b6dde9c9d2a92c742
iraes09 <alo3809050@gmail.com>
2025-03-27 18:58:29 +0100

    texto

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,14 +12,14 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lujoYel">
-                        {{ __('home') }}
+                    <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lujoYel text-decoration-none">
+                        {{ __('Home') }}
                     </x-nav-link>
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('tickets.index') }}" :active="request()->routeIs('tickets.index')" class="text-lujoYel">
-                        {{ __('tickets') }}
+                    <x-nav-link href="{{ route('tickets.index') }}" :active="request()->routeIs('tickets.index')" class="text-lujoYel text-decoration-none">
+                        {{ __('Tickets') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -145,8 +145,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                {{ __('home') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link href="{{ route('tickets.index') }}" :active="request()->routeIs('tickets.index')">
+                {{ __('tickets') }}
             </x-responsive-nav-link>
         </div>
 
