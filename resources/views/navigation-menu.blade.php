a09c3dec171063e595678a7269cbece6103368a5
iraes09 <alo3809050@gmail.com>
2025-03-27 14:36:51 +0100

    navegacion

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,8 +12,14 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                    <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')" class="text-lujoYel">
                         {{ __('home') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('tickets.index') }}" :active="request()->routeIs('tickets.index')" class="text-lujoYel">
+                        {{ __('tickets') }}
                     </x-nav-link>
                 </div>
             </div>
